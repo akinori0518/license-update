3e04fa43ee03b750602c55c0d87d7b4ca4866aa2
akinori0518 <aki.yamamoto0518@gmail.com>
2020-10-01 21:48:58 +0900

    commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,6 +266,8 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
+  config.scoped_views = true
+  
   config.sign_out_via = :delete
 
   # ==> OmniAuth
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_125441) do
+ActiveRecord::Schema.define(version: 2020_10_01_121049) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
